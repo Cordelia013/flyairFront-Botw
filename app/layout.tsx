6ab26e82fffbcec6842a5e30424399e17f2c00e1
Cordelia013 <cordeliaguims@yahoo.com>
2025-05-11 00:46:49 +0200

    add font
    Modification des polices dans globals.css pour utiliser des polices locales (PP Neue Montreal et PP Pangaia) au lieu des polices Geist. Mise à jour de layout.tsx pour intégrer ces nouvelles polices dans la mise en page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,38 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
 import "./globals.css";
 import "./mediaQueries.css";
 
 import ConfigurableGrid from "@/components/ConfigurableGrid";
- 
 
-// Définition des polices Geist
-const geistSans = Geist({
-  variable: "--font-geist-sans", 
-  subsets: ["latin"],
-  display: "swap",
+// Définition des polices locales
+const ppNeueMontreal = localFont({
+  src: [
+    {
+      path: '../public/font/pp-neue-montreal/PPNeueMontreal-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/pp-neue-montreal/PPNeueMontreal-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/pp-neue-montreal/PPNeueMontreal-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+  variable: '--font-pp-neue-montreal',
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap", 
+const ppPangaia = localFont({
+  src: '../public/font/pp-pangaia-webfont/PPPangaia-Regular.woff2',
+  variable: '--font-pp-pangaia',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -31,9 +46,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fr" className={`${ppNeueMontreal.variable} ${ppPangaia.variable}`}>
       <body className="min-h-screen w-screen">
-     
         <main>
           {children}
         </main>
