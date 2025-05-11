e9977b8f91ca2c4891b710667b5e7017a05815ac
Cordelia013 <cordeliaguims@yahoo.com>
2025-05-12 00:02:54 +0200

    Correction de l'ordre des classes de polices dans layout.tsx pour assurer un rendu correct des styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fr" className={`${ppNeueMontreal.variable} ${ppPangaia.variable}`}>
+    <html lang="fr" className={`${ppPangaia.variable} ${ppNeueMontreal.variable}`}>
       <body className="min-h-screen w-screen">
         <main>
           {children}
