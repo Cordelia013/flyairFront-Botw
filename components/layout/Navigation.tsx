3e3aaa6d5f2feacd0726b0759e48a2bb8a3db152
Cordelia013 <cordeliaguims@yahoo.com>
2025-05-14 00:29:53 +0200

    Ajout de nouvelles commandes dans package.json pour faciliter le déploiement avec Vercel. Mise à jour du lien vers la page des tours dans Navigation.tsx pour utiliser un chemin absolu.

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Tours from "../tours/Tours";
 
 
 export default function Navigation(){
     return (
         <div className=" ">
         <ul className="grid grid-cols-12 justify-end mt-8 text-[8px] md:text-[10px] lg:text-[12px] xl:text-[14px] 2xl:text-[16px]">
-            <li className="col-span-1 col-start-10"><Link href="./tours">TOURS</Link></li>
+            <li className="col-span-1 col-start-10"><Link href={"/tours"}>TOURS</Link></li>
             <li className="col-span-1"><Link href="./resa">RESERVATION</Link></li>
             <li className="col-span-1">LOREM</li>
         </ul>
